64a5bd2c0e606c2691d4e362e72d11701bf4c93f
Nicholas Knize <nknize@gmail.com>
2015-07-24 15:35:04 -0500

    adding native lucene GeoPointDistanceRangeQuery, ammending GeoPointBinaryDV to use new GeoPointField, test bug fixes still WIP

---
+++ core/src/main/java/org/apache/lucene/search/GeoPointDistanceRangeQuery.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to Elasticsearch under one or more contributor
+ * license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright
+ * ownership. Elasticsearch licenses this file to you under
+ * the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.lucene.search;
+
+import org.apache.lucene.index.IndexReader;
+
+/**
+ *
+ */
+public final class GeoPointDistanceRangeQuery extends GeoPointDistanceQuery {
+  protected final double minRadius;
+
+  public GeoPointDistanceRangeQuery(final String field, final double centerLon, final double centerLat,
+                                    final double minRadius, final double maxRadius) {
+    super(field, centerLon, centerLat, maxRadius);
+    this.minRadius = minRadius;
+  }
+
+  @Override
+  public Query rewrite(IndexReader reader) {
+    Query q = super.rewrite(reader);
+    if (minRadius == 0.0) {
+      return q;
+    }
+
+    // add an exclusion query
+    BooleanQuery bqb = new BooleanQuery();
+    GeoPointDistanceQuery exclude = new GeoPointDistanceQuery(field, centerLon, centerLat, minRadius);
+    bqb.add(new BooleanClause(q, BooleanClause.Occur.SHOULD));
+    bqb.add(new BooleanClause(exclude, BooleanClause.Occur.MUST_NOT));
+
+    return bqb;
+  }
+
+}
--- core/src/test/java/org/elasticsearch/search/geo/GeoDistanceIT.java
+++ core/src/test/java/org/elasticsearch/search/geo/GeoDistanceIT.java
@@ -65,11 +65,11 @@ import static org.hamcrest.Matchers.equalTo;
 public class GeoDistanceIT extends ESIntegTestCase {
 
     @Test
-    @AwaitsFix(bugUrl = "awaits full GeoPointField Integration")
     public void simpleDistanceTests() throws Exception {
         XContentBuilder xContentBuilder = XContentFactory.jsonBuilder().startObject().startObject("type1")
                 .startObject("properties").startObject("location").field("type", "geo_point").field("lat_lon", true)
-                .startObject("fielddata").field("format", randomNumericFieldDataFormat()).endObject().endObject().endObject()
+                .startObject("fielddata").field("format", randomNumericFieldDataFormat()).endObject().endObject()
+                .endObject()
                 .endObject().endObject();
         assertAcked(prepareCreate("test").addMapping("type1", xContentBuilder));
         ensureGreen();
@@ -219,7 +219,6 @@ public class GeoDistanceIT extends ESIntegTestCase {
     }
 
     @Test
-    @AwaitsFix(bugUrl = "awaits full GeoPointField Integration")
     public void testDistanceSortingMVFields() throws Exception {
         XContentBuilder xContentBuilder = XContentFactory.jsonBuilder().startObject().startObject("type1")
                 .startObject("properties").startObject("locations").field("type", "geo_point").field("lat_lon", true)
@@ -345,7 +344,6 @@ public class GeoDistanceIT extends ESIntegTestCase {
 
     @Test
     // Regression bug: https://github.com/elasticsearch/elasticsearch/issues/2851
-    @AwaitsFix(bugUrl = "awaits full GeoPointField Integration")
     public void testDistanceSortingWithMissingGeoPoint() throws Exception {
         XContentBuilder xContentBuilder = XContentFactory.jsonBuilder().startObject().startObject("type1")
                 .startObject("properties").startObject("locations").field("type", "geo_point").field("lat_lon", true)
@@ -639,7 +637,6 @@ public class GeoDistanceIT extends ESIntegTestCase {
      * Issue 3073
      */
     @Test
-    @AwaitsFix(bugUrl = "awaits full GeoPointField Integration")
     public void testGeoDistanceFilter() throws IOException {
         double lat = 40.720611;
         double lon = -73.998776;
@@ -650,12 +647,6 @@ public class GeoDistanceIT extends ESIntegTestCase {
                         .startObject("properties")
                             .startObject("pin")
                                 .field("type", "geo_point")
-//                                .field("geohash", true)
-//                                .field("geohash_precision", 24)
-//                                .field("lat_lon", true)
-//                                .startObject("fielddata")
-//                                    .field("format", randomNumericFieldDataFormat())
-//                                .endObject()
                             .endObject()
                         .endObject()
                     .endObject()
